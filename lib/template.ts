d30cca6d21f3e361a1fcdf4c16e63a04ab85cd8f
Aditya Pranjal <srivastavatubbu@gmail.com>
2025-12-24 23:46:35 +0530

    monaco editor , header and sub header part done

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+  REACT: "/scripts.ai-starters/react-ts",
+  NEXTJS: "/scripts.ai-starters/nextjs-new",
+  EXPRESS: "/scripts.ai-starters/express-simple",
+  VUE: "/scripts.ai-starters/vue",
+  HONO: "/scripts.ai-starters/hono-nodejs-starter",
+  ANGULAR: "/scripts.ai-starters/angular",
+};
